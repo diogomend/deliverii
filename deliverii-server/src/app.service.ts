304dfa5d840d35a6c5d306ff957ea618ba409229
Diogo.Mendonca <diogo.mendonca@hostelworld.com>
2020-09-22 12:37:40 +0100

    API main page refactoring

--- deliverii-server/src/app.service.ts
+++ deliverii-server/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 /* istanbul ignore file */
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getInit() {
+    return {api: "Deliverii API", version: 1.0};
   }
 }
